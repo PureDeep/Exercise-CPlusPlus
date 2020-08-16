4e267691f8c962865dc630d058470a3d26305383
PureDeep <542057950@qq.com>
2020-08-16 17:05:16 +0800

    Forth

---
+++ 05Cin.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // //1.int
+    // int a = 0;
+    // cout << "1. int : " << endl;
+    // cin >> a;
+    // cout << "int a : " << a << endl;
+    // //2.float
+    // double b = 0.0f;
+    // cout << "2. float : " << endl;
+    // cin >> b;
+    // cout << "float b : " << b << endl;
+    // //3.char
+    // char c = 'a';
+    // cin >> c;
+    // cout << "char : " << c << endl;
+    // string str = "Hello World!";
+    // cout << str << endl;
+    // cin >> str;
+    // cout << str << endl;
+    bool flag = false;
+    cin >> flag;
+    cout << flag << endl;
+    return 0;
+}
