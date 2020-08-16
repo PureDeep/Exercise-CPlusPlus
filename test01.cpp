34a36e7ddfd47805a1b33db25567b9b7bc15434f
PureDeep <542057950@qq.com>
2020-08-16 12:08:46 +0800

    First C++ Ex

---
+++ test01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    short num1 = 10;
+    cout << "num1: " << num1 << endl;
+    cout << "size of num1: " << sizeof(num1) << endl;
+    int num2 = 10;
+    cout << "num2: " << num2 << endl;
+    cout << "size of num2: " << sizeof(num2) << endl;
+    return 0;
+}
