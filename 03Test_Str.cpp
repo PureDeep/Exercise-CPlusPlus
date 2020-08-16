586fb250c3f8bbc96da8e940b03c89116b53a485
PureDeep <542057950@qq.com>
2020-08-16 16:01:49 +0800

    third

---
+++ 03Test_Str.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    //C风格字符串
+    char str[] = "Hello World!";
+    cout << str << endl;
+    //C++风格字符串
+    string str2 = "hello world!";
+    cout << str2 << endl;
+    return 0;
+}
---
+++ 04Test_Bool.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    bool flag = true;
+    cout << "bool flag : " << flag << endl;
+    flag = false;
+    cout << "false flag : " << flag << endl;
+    cout << "size of bool : " << sizeof(flag) << endl;
+    return 0;
+}
