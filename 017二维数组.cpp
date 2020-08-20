fb71ae3affbe2f18ee1b82cf3f4c78d033442e9b
PureDeep <542057950@qq.com>
2020-08-20 17:14:09 +0800

    8.20_2

---
+++ 017二维数组.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: PureDeep
+ * @Date: 2020-08-20 16:30:42
+ * @LastEditors: PureDeep
+ * @LastEditTime: 2020-08-20 16:34:45
+ * @FilePath: \C++\ExerciseC++\017ถþฮฌสýื้.cpp
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[2][3] = {1, 2, 3, 4, 5, 6};
+    cout << arr << endl;
+    cout << &arr[0][0] << endl;
+    return 0;
+}
---
+++ 018考试成绩统计.cpp
@@ -0,0 +1,25 @@
+/*
+ * @Author: PureDeep
+ * @Date: 2020-08-20 16:38:33
+ * @LastEditors: PureDeep
+ * @LastEditTime: 2020-08-20 16:49:03
+ * @FilePath: \C++\ExerciseC++\018øº ‘≥…º®Õ≥º∆.cpp
+ */
+#include<iostream>
+using namespace std;
+
+int main(){
+    int scores[3][3] = {
+        {100,100,100},
+        {90,50,100},
+        {60,70,80}
+    };
+    for(int i = 0; i < 3; i++){
+        int sum = 0;
+        for(int j = 0; j < 3; j++){
+            sum += scores[i][j];
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
