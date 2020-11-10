4d1b0b3e393b1ae8cd53f6e474db0381cce8a366
PureDeep <542057950@qq.com>
2020-11-10 20:03:19 +0800

    2020-11-10-1

--- 129mstore.cpp
+++ 129mstore.cpp
@@ -2,9 +2,13 @@
  * @Author: PureDeep
  * @Date: 2020-10-29 11:26:56
  * @LastEditors: PureDeep
- * @LastEditTime: 2020-10-29 11:27:48
+ * @LastEditTime: 2020-10-29 11:28:56
  * @FilePath: \Exercise-CPlusPlus\129mstore.cpp
  */
 long mult2(long, long);
 
-void
+void multStore(long x, long y, long *dest)
+{
+    long t = mult2(x, y);
+    *dest = t;
+}
