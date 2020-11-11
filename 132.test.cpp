900911a388e2935984250b05f40c36652c0a4877
PureDeep <542057950@qq.com>
2020-11-11 16:42:30 +0800

    2020-11-11-1

---
+++ 132.test.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: PureDeep
+ * @Date: 2020-11-11 12:03:39
+ * @LastEditors: PureDeep
+ * @LastEditTime: 2020-11-11 12:05:19
+ * @FilePath: \Exercise-CPlusPlus\132.test.cpp
+ */
+#pragma GCC diagnostic error "-std=c++11"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello c++11" << endl;
+    return 0;
+}
